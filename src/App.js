4e9b24eab65b493f1223d941418a5e805d24d7b3
OckJuYong <20201776@edu.hanbat.ac.kr>
2023-11-26 17:11:22 +0900

    data sum plus

--- src/App.js
+++ src/App.js
@@ -1,10 +1,8 @@
 import './App.css';
+import data from './data/data.json';
 import { useEffect } from 'react';
 
 function App() {
-
-  
-  // 스크립트 파일 읽어오기
   const new_script = (src) => {
     return new Promise((resolve, reject) => {
       const script = document.createElement('script');
@@ -20,18 +18,12 @@ function App() {
   };
 
   useEffect(() => {
-
-    
-    
-    // 카카오맵 스크립트 읽어오기
-    const my_script = new_script(
-      'https://dapi.kakao.com/v2/maps/sdk.js?autoload=false&appkey=8cd203fb8326a745f18faf8e43268481'
-    );
-  
-    // 스크립트 읽기 완료 후 카카오맵 설정
-    my_script.then(() => {
-      console.log('script loaded!!!');
+    const initializeMap = async () => {
       const kakao = window.kakao;
+      await new_script(
+        'https://dapi.kakao.com/v2/maps/sdk.js?autoload=false&appkey=8cd203fb8326a745f18faf8e43268481'
+      );
+
       kakao.maps.load(() => {
         const mapContainer = document.getElementById('map');
         const options = {
@@ -39,39 +31,22 @@ function App() {
           level: 3,
         };
         const map = new kakao.maps.Map(mapContainer, options);
-  
-        // 이미지의 경로 설정
-        const imageSrc = '/images/kakao_Img.png';
-        const imagesrc = '/images/kim.jpg';
-  
-        // 마커 랜드마크 태그 설정
-        const markerimage = new kakao.maps.MarkerImage(imageSrc, new kakao.maps.Size(50, 50));
-        const marker_image = new kakao.maps.MarkerImage(imagesrc, new kakao.maps.Size(50, 50));
-        const markerPosition1 = new kakao.maps.LatLng(36.35120, 127.3017);
-        const markerPosition2 = new kakao.maps.LatLng(36.34987, 127.2981);
 
-        // 내 위치 태그
-        const myposition = new kakao.maps.LatLng(36.352087984022205, 127.30028523386005);
-  
-        const marker1 = new kakao.maps.Marker({
-          position: markerPosition1,
-          image: markerimage,
-        });
-  
-        const marker2 = new kakao.maps.Marker({
-          position: markerPosition2,
-          image: marker_image,
-        });
+        data.addresses.forEach(({ latitude, longitude, name, image }) => {
+          const markerPosition = new kakao.maps.LatLng(latitude, longitude);
+          const markerImage = image ? new kakao.maps.MarkerImage(image, new kakao.maps.Size(50, 50)) : null;
+          const marker = new kakao.maps.Marker({
+            position: markerPosition,
+            title: name, // 이름 추가
+            image: markerImage
+          });
 
-        const my_marker = new kakao.maps.Marker({
-          position: myposition
+          marker.setMap(map);
         });
-  
-        marker1.setMap(map);
-        marker2.setMap(map);
-        my_marker.setMap(map);
       });
-    });
+    };
+
+    initializeMap();
   }, []);
 
   return (
