098070d155b1f1b666c371dd484012ff47d76d65
OckJuYong <20201776@edu.hanbat.ac.kr>
2023-12-02 19:18:03 +0900

    last project

--- src/App.js
+++ src/App.js
@@ -1,20 +1,22 @@
+import React, { useState, useEffect } from 'react';
 import './App.css';
-import data from './data/data.json'; // 더미데이터 DB 불러오기
-import { useEffect } from 'react';
+import data from './data/data.json';
+import quz from './data/qu.json';
 
 const myaddress = {
   latitude: 36.352087984022205,
   longitude: 127.30028523386005,
-}
+};
+
 function distance(lat1, lon1, lat2, lon2) {
-  const R = 6371; // Earth radius in kilometers
+  const R = 6371;
   const dLat = deg2rad(lat2 - lat1);
   const dLon = deg2rad(lon2 - lon1);
   const a = Math.sin(dLat / 2) * Math.sin(dLat / 2) +
     Math.cos(deg2rad(lat1)) * Math.cos(deg2rad(lat2)) *
     Math.sin(dLon / 2) * Math.sin(dLon / 2);
   const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
-  const distance = R * c; // Distance between the two points in kilometers
+  const distance = R * c;
   return distance;
 }
 
@@ -22,15 +24,16 @@ function deg2rad(deg) {
   return deg * (Math.PI / 180);
 }
 
-const MyLocation = {
-  latitude: myaddress.latitude,
-  longitude: myaddress.longitude,
-};
-
-
-
-
 function App() {
+  const [userAnswer, setUserAnswer] = useState('');
+  const [isAnswerCorrect, setIsAnswerCorrect] = useState(null);
+  const [currentQuiz, setCurrentQuiz] = useState(null);
+  const [isQuizVisible, setIsQuizVisible] = useState(false);
+  const [MyLocation, setMyLocation] = useState({
+    latitude: myaddress.latitude,
+    longitude: myaddress.longitude,
+  });
+
   const new_script = (src) => {
     return new Promise((resolve, reject) => {
       const script = document.createElement('script');
@@ -44,8 +47,40 @@ function App() {
       document.head.appendChild(script);
     });
   };
-  // 카카오맵 라이브러리 가져오기
+
+  const checkAnswer = (quiz, userAnswer) => {
+    return quiz.answer === parseInt(userAnswer);
+  };
+
+  const getRandomQuiz = () => {
+    const randomIndex = Math.floor(Math.random() * quz.S2art.length);
+    return quz.S2art[randomIndex];
+  };
+
+  const handleQuizAnswer = () => {
+    if (userAnswer !== '') {
+      if (checkAnswer(currentQuiz, userAnswer)) {
+        setIsAnswerCorrect(true);
+      } else {
+        setIsAnswerCorrect(false);
+      }
+    } else {
+      setIsAnswerCorrect(null);
+    }
+  };
+
+  const handleInputChange = (e) => {
+    setUserAnswer(e.target.value);
+  };
+
+  const handleAnswerSubmit = () => {
+    handleQuizAnswer();
+  };
+
   useEffect(() => {
+    const randomQuiz = getRandomQuiz();
+    setCurrentQuiz(randomQuiz);
+
     const initializeMap = async () => {
       const kakao = window.kakao;
       await new_script(
@@ -54,40 +89,75 @@ function App() {
 
       kakao.maps.load(() => {
         const mapContainer = document.getElementById('map');
-        const options = //내 좌표값 설정 (가운데 정렬)
-        {
-          center: new kakao.maps.LatLng(myaddress.latitude, myaddress.longitude),
+        const options = {
+          center: new kakao.maps.LatLng(MyLocation.latitude, MyLocation.longitude),
           level: 3,
         };
         const map = new kakao.maps.Map(mapContainer, options);
-        // 태그밑 이미지 설정
+
         data.addresses.forEach(({ latitude, longitude, name, image }) => {
           const markerPosition = new kakao.maps.LatLng(latitude, longitude);
-          const markerImage = image ? new kakao.maps.MarkerImage(image, new kakao.maps.Size(50, 50)) : null;  // 이미지 띄우기
+          const markerImage = image ? new kakao.maps.MarkerImage(image, new kakao.maps.Size(50, 50)) : null;
           const marker = new kakao.maps.Marker({
             position: markerPosition,
-            title: name, // 이름 추가
-            image: markerImage  // 이미지 추가
+            title: name,
+            image: markerImage
           });
           const markerDistance = distance(MyLocation.latitude, MyLocation.longitude, latitude, longitude);
-          // console.log(`Distance to ${name}: ${markerDistance.toFixed(2)} km`);
-          // 내위치와 태그위치 거리를 계산하여 내위치가 아니고 태그와 가까울 때 밑의 코드가 실행
+
           if (markerDistance.toFixed(2) < 0.05 && name !== "내위치" && name !== "imsi 내위치") {
             console.log(`${name} 사진을 찍을 수 있습니다.`);
+            setIsQuizVisible(true);
           }
 
-          // 태그 띄우는 부분
           marker.setMap(map);
         });
       });
     };
 
+    const updateGPSLocation = () => {
+      navigator.geolocation.getCurrentPosition(
+        (position) => {
+          const { latitude, longitude } = position.coords;
+          setMyLocation({
+            latitude,
+            longitude,
+          });
+        },
+        (error) => {
+          console.error('Error getting geolocation:', error);
+        }
+      );
+    };
+
+    // 최초 한 번 실행
+    updateGPSLocation();
+
+    // 주기적으로 위치 업데이트를 위한 setInterval 설정
+    const gpsUpdateInterval = setInterval(updateGPSLocation, 10000);
+
     initializeMap();
-  }, []);
+
+    return () => clearInterval(gpsUpdateInterval);
+  }, [MyLocation.latitude, MyLocation.longitude]);
 
   return (
     <div className="App">
       <div id="map" className="map"></div>
+      <div>
+        {isQuizVisible && currentQuiz && (
+          <>
+            <p>문제: {currentQuiz.question}</p>
+            <input type="text" value={userAnswer} onChange={handleInputChange} />
+            <button onClick={handleAnswerSubmit}>정답 확인</button>
+            {isAnswerCorrect !== null && (
+              <div>
+                {isAnswerCorrect ? '정답입니다!' : '틀렸습니다!'}
+              </div>
+            )}
+          </>
+        )}
+      </div>
     </div>
   );
 }
